ce3989e4904206cf5996056c4dcb9b5c66e2bd4c
Byron <bmayne@ea.com>
2021-12-31 17:46:25 -0500

    Added new test project

---
+++ src/Recoil.net.Tests/SelectorTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Recoil.net.Tests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Create_WithNullKey_ThrowsException()
+		{
+
+		}
+	}
+}
