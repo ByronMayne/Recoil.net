6762784cdc69ecbb37af490abbf15b6b40750bd3
Byron <bmayne@ea.com>
2022-01-03 18:20:45 -0500

    Changed RecoilRoot to a ContentControl It was a user control before which would break named elements.

--- src/Recoil.net/RecoilRoot.xaml.cs
+++ src/Recoil.net/RecoilRoot.xaml.cs
@@ -7,7 +7,7 @@ namespace RecoilNet
     /// <summary>
     /// Interaction logic for RecoilRoot.xaml
     /// </summary>
-    public partial class RecoilRoot : UserControl
+    public partial class RecoilRoot : ContentControl
     {
         /// <summary>
         /// Gets or sets the recoil store to use
@@ -18,9 +18,25 @@ namespace RecoilNet
             set { SetValue(StoreProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty StoreProperty =
-            DependencyProperty.RegisterAttached(
+        /// <summary>
+        /// Defaults to true. This prop only matters when this <RecoilRoot> is nested within another <RecoilRoot>.
+        /// If override is true, this root will create a new Recoil scope. If override is false, this <RecoilRoot> 
+        /// will perform no function other than rendering its children, thus children of this root will access the 
+        /// Recoil values of the nearest ancestor RecoilRoot.
+        /// </summary>
+        public bool Override
+        {
+            get => (bool)GetValue(OverrideProperty);
+            set => SetValue(OverrideProperty, value);
+        }
+
+
+        public static readonly DependencyProperty OverrideProperty =
+			DependencyProperty.Register(nameof(Override), typeof(bool), 
+                typeof(RecoilRoot), new PropertyMetadata(true));
+
+		public static readonly DependencyProperty StoreProperty =
+            DependencyProperty.Register(
                 nameof(Store),
                 typeof(RecoilStore),
                 typeof(RecoilRoot),
