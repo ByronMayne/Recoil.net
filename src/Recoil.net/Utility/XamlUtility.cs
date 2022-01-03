b5f85f582deb99be44306d8ad22979763f83500d
Byron <bmayne@ea.com>
2022-01-03 17:12:08 -0500

    RecoilRoot can now be found as a direct child of the element requesting it. This is used for Pages and windows to not have to created a nested control.

--- src/Recoil.net/Utility/XamlUtility.cs
+++ src/Recoil.net/Utility/XamlUtility.cs
@@ -1,6 +1,8 @@
 ﻿using RecoilNet.State;
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RecoilNet.Utility
 {
@@ -15,6 +17,27 @@ namespace RecoilNet.Utility
 		public static IRecoilStore GetRecoilStore(FrameworkElement element)
 		{
 			ArgumentNullException.ThrowIfNull(element);
+
+			// We allow windows and pages to use their direct child as stores 
+			switch (element)
+			{
+				case ContentControl contentControl:
+					{
+						if (contentControl.Content is RecoilRoot recoilRoot)
+						{
+							return recoilRoot.Store;
+						}
+					}
+					break;
+				case Page asPage:
+					{
+						if (asPage.Content is RecoilRoot recoilRoot)
+						{
+							return recoilRoot.Store;
+						}
+					}
+					break;
+			}
 
 			RecoilRoot? root = element.FindTypeInAncestors<RecoilRoot>();
 
