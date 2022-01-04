f6af1dee3923f9cac75c3c815ef219bfaf7ad9dd
Byron <bmayne@ea.com>
2022-01-04 11:10:15 -0500

    Added helper for using a RecoilState DataContext It's an extension method for Framework Element

--- src/Recoil.net/TypeExtensions/FrameworkElementExtensions.cs
+++ src/Recoil.net/TypeExtensions/FrameworkElementExtensions.cs
@@ -10,6 +10,8 @@ namespace RecoilNet
 	/// </summary>
 	public static class FrameworkElementExtensions
 	{
+		private const string RECOIL_STATE_RESOURCE_KEY = $"{nameof(RecoilState)}_DataContext";
+
 		/// <summary>
 		/// Invokes an action right away on a framework element if it's loaded otherwise will invoke after it's loaded
 		/// </summary>
@@ -57,6 +59,47 @@ namespace RecoilNet
 
 
 		/// <summary>
+		/// Configures the sent in <see cref="FrameworkElement"/> property <see cref="FrameworkElement.DataContext"/> to be in
+		/// sync with the given <see cref="Atom{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">The value type</typeparam>
+		/// <param name="element">The element to set the data context for</param>
+		/// <param name="atom">The Atom to link to the context</param>
+		public static void UseRecoilStateDataContext<T>(this FrameworkElement element, Atom<T> atom)
+			=> UseRecoilStateDataContextInternal<T>(element, atom);
+
+		/// <summary>
+		/// Configures the sent in <see cref="FrameworkElement"/> property <see cref="FrameworkElement.DataContext"/> to be in
+		/// sync with the given <see cref="Selector{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">The value type</typeparam>
+		/// <param name="element">The element to set the data context for</param>
+		/// <param name="selector">The Selector to link to the context</param>
+		public static void UseRecoilStateDataContext<T>(this FrameworkElement element, Selector<T> selector)
+			=> UseRecoilStateDataContextInternal<T>(element, selector);
+
+		/// <summary>
+		/// Removes the binding of a <see cref="RecoilState"/> to the framework elements <see cref="FrameworkElement.DataContext"/>
+		/// property. This is only seee if you used <see cref="UseRecoilStateDataContext{T}(FrameworkElement, Atom{T})"/> or <see cref="UseRecoilStateDataContext{T}(FrameworkElement, Selector{T})"/>
+		/// </summary>
+		/// <param name="element"></param>
+		public static bool RemoveRecoilStateDataContext(this FrameworkElement element)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+
+			if (element.Resources.Contains(RECOIL_STATE_RESOURCE_KEY))
+			{
+				RecoilState recoilState = (RecoilState)element.Resources[RECOIL_STATE_RESOURCE_KEY];
+				recoilState.Dispose();
+				element.Resources.Remove(RECOIL_STATE_RESOURCE_KEY);
+				return true;
+			}
+			return false;
+		}
+
+
+
+		/// <summary>
 		/// Once the control has been initialized thie will create a new instance the ViewModel of type T and assing it to the framework elements view model
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
@@ -98,7 +141,22 @@ namespace RecoilNet
 		}
 
 
+		/// <summary>
+		/// Creates a new <see cref="RecoilState"/> and attaches it to a <see cref="FrameworkElement"/> in it's resources. It then subscribes
+		/// to the value change event which will set the data contexts value.
+		/// </summary>
 
+		private static void UseRecoilStateDataContextInternal<T>(FrameworkElement element, RecoilValue<T> value)
+		{
+			RecoilState<T> recoilState = UseRecoilStateInternal(element, value);
+			element.Resources[RECOIL_STATE_RESOURCE_KEY] = recoilState;
+			element.DataContext = recoilState.Value;
+			recoilState.ValueChanged += (sender, value) => element.DataContext = value;
+		}
+
+		/// <summary>
+		/// Creates a new recoil state for the given framework element
+		/// </summary>
 		private static RecoilState<T> UseRecoilStateInternal<T>(FrameworkElement element, RecoilValue<T> recoilValue, RecoilStateOptions stateOptions = RecoilStateOptions.Default)
 		{
 			RecoilState<T> state = new RecoilState<T>(recoilValue, null);
