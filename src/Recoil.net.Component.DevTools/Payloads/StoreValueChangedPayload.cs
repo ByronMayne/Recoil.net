70aefbf19c33bece2909f5bff06ede0a498403f9
Byron <bmayne@ea.com>
2022-01-04 11:12:28 -0500

    Created a new Recoil.net.Component.DevTools This is the logic that allows the DevTools application to connect and read values.

---
+++ src/Recoil.net.Component.DevTools/Payloads/StoreValueChangedPayload.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recoil.Components.DevTools.Payloads
+{
+	internal record StoreValueChangedPayload
+	{
+		/// <summary>
+		/// Gets the unique id of the store that raised the event
+		/// </summary>
+		public int StoreId { get; init; }
+
+		/// <summary>
+		/// Gets the key that the value belongs too
+		/// </summary>
+		public string? Key { get; init; }
+
+		/// <summary>
+		/// Gets the list of the keys that have been changed because they depended
+		/// on the <see cref="Key"/>
+		/// </summary>
+		public IReadOnlyList<string>? DependentKeys { get; init; }
+
+		/// <summary>
+		/// Gets the type of the value that changed 
+		/// </summary>
+		public string? ValueTypeName { get; init; }
+
+		/// <summary>
+		/// Gets or sets if the change had a value or if it was null
+		/// </summary>
+		public bool HasValue { get; init; }
+
+		/// <summary>
+		/// Gets the json string of the object that changed 
+		/// </summary>
+		public string? JsonValue { get; init; }
+	}
+}
---
+++ src/Recoil.net.Component.DevTools/TypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Recoil.Components.DevTools;
+using RecoilNet;
+
+namespace RecoilNet
+{
+	/// <summary>
+	/// Contains extension methods for adding in the dev tools
+	/// </summary>
+	public static class TypeExtensions
+	{
+		/// <summary>
+		/// Adds the development tools component to the generated store. This component allows the running application to be
+		/// inspected by the Recoil.net.DevTools application.
+		/// </summary>
+		public static StoreConfigurationBuilder AddDevelopmentTools(this StoreConfigurationBuilder builder)
+		{
+			builder.AddComponent<DevToolsComponent>();
+			return builder;
+		}
+	}
+}
